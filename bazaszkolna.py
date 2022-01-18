0c9607df932c75e93fcc51ccef8848d8a2b111ee
Kamil <kamilrusztyn@gmail.com>
2022-01-18 18:31:16 +0100

    gotowy program

--- bazaszkolna.py
+++ bazaszkolna.py
@@ -2,109 +2,19 @@ import sys
 
 operacje = ("wychowawca", "nauczyciel", "uczen", "koniec")
 
-wychowawcy = []
-nauczyciele = []
-uczniowie = []
+#baza={}
+wychowawcy = {}
+nauczyciele = {}
+uczniowie = {}
 grupy = {}
 
-
-def wydruk(argument):
-    if argument in wychowawcy:
-        print(wychowawcy)
-    if argument in nauczyciele:
-        print(nauczyciele)
-    if argument in uczniowie:
-        print(uczniowie)
-    if argument in klasy: 
-        print(f"wychowawca:\n{klasy[argument][0]} \nuczniowie:")
-        for osoba in klasy[argument][1:]:
-            print(osoba)
-
-
-class Grupa:
-    def __init__(self,numer):
-        self.numer = numer
-        self.wychowawca = None
-        self.nauczyciele = []
-        self.uczniowie  = []
-
-def dodanie_grupy(numer):
-    if numer not in grupy:
-        grupa = Grupa(numer)
-        grupy[numer] = grupa  
-    return grupy[numer]           
-
-
-class Wychowawca:
-    def __init__(self):
-        self.imie = ""
-        self.klasy = []
-
-    def pobor_danych(self):
-        self.imie = input()
-        wychowawcy.append(self.imie)
-        klasy = []
-        while True:
-            klasa = input()
-            if not klasa:
-                break    
-            self.klasy.append(klasa)
-            #grupa = dodanie_grupy(self.klasa)
-            #grupa.wychowawca = self      
-
-
-class Nauczyciel:
-    def __init__(self):
-        self.imie = ""
-        self.przedmiot = ""
-        self.klasy = []
-
-    def pobor_danych(self):
-        self.imie = input()
-        nauczyciele.append(self.imie)
-        self.przedmiot = input()
-        klasy = []
-        while True:
-            klasa = input()
-            if not klasa:
-                break    
-            self.klasy.append(klasa)
-            #grupa = dodanie_grupy(self.klasa)
-            #grupa.nauczyciele.append(self)
-
-
-class Uczen:
-    def __init__(self):
-        self.imie = ""
-        self.klasa = ""
-
-    def pobor_danych(self):
-        self.imie = input()
-        uczniowie.append(self.imie)
-        self.klasa = input()    
-        grupa = dodanie_grupy(self.klasa)
-        grupa.uczniowie.append(self)
-
-while True:
-    typ = input()
-    if typ not in operacje:
-        print("blad")
-        continue
-    if typ == "wychowawca":
-        osoba = Wychowawca()
-    if typ == "nauczyciel":
-        osoba = Nauczyciel()
-    if typ == "uczen":
-        osoba = Uczen()        
-    if typ == "koniec":
-        break 
-    osoba.pobor_danych()
-
-
 if len(sys.argv) !=2:
-    print("Nie podano prawidłowej funkcji programu")    
+    print("Nie podano prawidłowej funkcji programu")  
+    phrase = None
 else:
-    komenda = sys.argv[1]
+    phrase = sys.argv[1]
+    
+def wydruk(komenda):
     if komenda in grupy:
         #print(grupy[komenda])
         print("klasa")
@@ -115,7 +25,129 @@ else:
     if komenda in uczniowie:
         print("uczen")
 
-#print(dir(grupy))
 
-#print(grupy.__contains__)
+class Grupa:
+    def __init__(self,numer):
+        self.numer = numer
+        self.wychowawca = None
+        self.nauczyciele = []
+        self.uczniowie  = []
+    
+    def wydruk(self):
+        print(f"wychowawcą klasy {phrase} jest {self.wychowawca.imie}")
+        print("uczęszczają do niej uczniowie:")
+        for uczen in self.uczniowie:
+            print(f"- {uczen.imie}")
 
+
+def dodanie_grupy(numer):
+    if numer not in grupy:
+        grupa = Grupa(numer)
+        grupy[numer] = grupa  
+    return grupy[numer]    
+
+
+
+class Wychowawca:
+    def __init__(self):
+        self.imie = ""
+        self.klasy = []
+
+    def pobor_danych(self):
+        self.imie = input()
+        while True:
+            klasa = input()
+            if not klasa:
+                break    
+            self.klasy.append(klasa)
+            grupa = dodanie_grupy(klasa)
+            grupa.wychowawca = self  
+
+    def wydruk(self): 
+        for klasa in self.klasy:
+            print(f"\n{self.imie} jest wychowawcą klasy {klasa},\nuczniowie:")
+            for uczen in grupy[klasa].uczniowie:
+                print(f"- {uczen.imie}")
+
+
+class Nauczyciel:
+    def __init__(self):
+        self.imie = ""
+        self.przedmiot = ""
+        self.klasy = []
+
+    def pobor_danych(self):
+        self.imie = input()
+        self.przedmiot = input()
+        while True:
+            klasa = input()
+            if not klasa:
+                break    
+            self.klasy.append(klasa)
+            grupa = dodanie_grupy(klasa)
+            grupa.nauczyciele.append(self)
+
+    def wydruk(self): 
+        for klasa in self.klasy:
+            if grupy[klasa].wychowawca:
+                print(f"wychowawca klasy {klasa} - {grupy[klasa].wychowawca.imie}")
+            else:
+                print(f"klasa {klasa} nie ma wychowawcy")
+
+class Uczen:
+    def __init__(self):
+        self.imie = ""
+        self.klasa = ""
+
+    def pobor_danych(self):
+        self.imie = input()
+        self.klasa = input()    
+        grupa = dodanie_grupy(self.klasa)
+        grupa.uczniowie.append(self)
+
+    def wydruk(self): 
+        for nauczyciel in grupy[self.klasa].nauczyciele:
+            print(f"\n ucznia {phrase} prowadzą:")
+            print(f"- {nauczyciel.imie} - {nauczyciele[nauczyciel.imie].przedmiot}")
+
+
+
+while True:         #główna pętla
+    typ = input()
+    if typ not in operacje:
+        print("blad")
+        continue
+    if typ == "koniec":
+        break 
+    if typ == "wychowawca":
+        osoba = Wychowawca()
+        osoba.pobor_danych()
+        wychowawcy[osoba.imie]=osoba
+    if typ == "nauczyciel":
+        osoba = Nauczyciel()
+        osoba.pobor_danych()
+        nauczyciele[osoba.imie]=osoba
+    if typ == "uczen":
+        osoba = Uczen() 
+        osoba.pobor_danych() 
+        uczniowie[osoba.imie]=osoba
+
+"""    osoba.pobor_danych()
+    if not baza.get(osoba.imie):
+        baza[osoba.imie]=osoba
+    baza[osoba.imie+"2"]=osoba"""
+
+
+if phrase in nauczyciele:
+    nauczyciele[phrase].wydruk()
+if phrase in wychowawcy:
+    wychowawcy[phrase].wydruk()
+if phrase in uczniowie:
+    uczniowie[phrase].wydruk()
+if phrase in grupy:
+    grupy[phrase].wydruk()
+
+#print(nauczyciele["Stanislaw Lem"].przedmiot)
+#print(grupy["1a"].nauczyciele)
+
+#print(uczniowie)      
